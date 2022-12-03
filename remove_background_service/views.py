bbe3bd822fa50f8ff7a8d2628d7466f2f1599099
lemanh-tuong <letuong05092000@gmail.com>
2022-12-03 22:30:56 +0700

    chay dc lan 1

--- remove_background_service/views.py
+++ remove_background_service/views.py
@@ -3,6 +3,11 @@ from rembg import remove
 import cv2
 from django.conf import settings
 
+input_path = settings.MEDIA_ROOT + '/1.jpg'
+output_path = 'output.png'
+input = cv2.imread(input_path)
+output = remove(input)
+cv2.imwrite(output_path, output)
 
 # Create your views here.
 def index(request):
