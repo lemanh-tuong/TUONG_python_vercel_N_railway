d008408d181a76a9c5f224ead1488449ff5b343b
lemanh-tuong <letuong05092000@gmail.com>
2022-12-05 14:55:50 +0700

    build lan 5

--- remove_background_service/views.py
+++ remove_background_service/views.py
@@ -5,6 +5,16 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def Demo(request):
+  if request.method == 'POST' or request.method == 'PUT':
+    try:
+      image = request.FILES['image'].read()
+      if image is not None:
+        output = remove(image)
+        return HttpResponse(output, content_type="image/jpeg")
+      else:
+        return HttpResponseBadRequest("Something went wrong!!!")
+    except:
+      return HttpResponseBadRequest("Something went wrong!!!")
   return render(request, 'demo.html')
 
 @csrf_exempt
@@ -12,8 +22,11 @@ def Service(request):
   if request.method == 'POST' or request.method == 'PUT':
     try:
       image = request.FILES['image'].read()
-      output = remove(image)
-      return HttpResponse(output, content_type="image/jpeg")
+      if image is not None:
+        output = remove(image)
+        return HttpResponse(output, content_type="image/jpeg")
+      else:
+        return HttpResponseBadRequest("Something went wrong!!!")
     except:
       return HttpResponseBadRequest("Something went wrong!!!")
       
