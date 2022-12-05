9195fa907b6c44758ec952e6e6c8cf446de5b5a4
lemanh-tuong <letuong05092000@gmail.com>
2022-12-05 14:47:03 +0700

    build lan 4

--- remove_background_service/views.py
+++ remove_background_service/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from rembg import remove
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def Demo(request):
   return render(request, 'demo.html')
 
+@csrf_exempt
 def Service(request):
   if request.method == 'POST' or request.method == 'PUT':
     try:
