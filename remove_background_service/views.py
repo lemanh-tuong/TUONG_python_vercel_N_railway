8da34bfecb0b2c6eea601c766114c65fd7bee883
lemanh-tuong <letuong05092000@gmail.com>
2023-01-06 20:40:57 +0700

    crop transparent

--- remove_background_service/views.py
+++ remove_background_service/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from rembg import remove
 from django.views.decorators.csrf import csrf_exempt
+from .utils import autocrop_image
+import io
 
 # Create your views here.
 def Demo(request):
@@ -30,3 +32,14 @@ def Service(request):
     except:
       return HttpResponseBadRequest("Something went wrong!!!")
       
+
+# Create your views here.
+def index(request):
+  if request.method == 'POST':
+    image = request.FILES['image'].read()
+    output_rembg = remove(image, True, 240, 10, 15)
+    output_crop = autocrop_image(output_rembg)
+    output = io.BytesIO()
+    output_crop.save(output, format="PNG")
+    return HttpResponse(output.getvalue(), content_type="image/png")
+  return render(request, 'index.html')
